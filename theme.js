7cb6fca4d584f14bb8d8bbf97eafad3cf303edfb
Souhaib-Hammami <144950412+Souhaib-Hammami@users.noreply.github.com>
2023-11-23 19:28:27 +0100

    4

--- theme.js
+++ theme.js
@@ -1,6 +1,10 @@
 
 function theme2() {
+<<<<<<< Updated upstream
     var theme = document.getElementById('R');
+=======
+    var theme = document.getElementById("R");
+>>>>>>> Stashed changes
     theme.src = "sky.jpg";
     return (theme);
 }
